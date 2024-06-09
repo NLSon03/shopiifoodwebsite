f549d4d5f5a7febd5780d674e139f06e97e4dfde
NLSon03 <nlson.2003.nguyen@gmail.com>
2024-06-09 22:24:08 +0700

    Initial commit

---
+++ src/main/java/com/group6b/shopiifoodwebsite/ShopiifoodwebsiteApplication.java
@@ -0,0 +1,13 @@
+package com.group6b.shopiifoodwebsite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopiifoodwebsiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShopiifoodwebsiteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/group6b/shopiifoodwebsite/ShopiifoodwebsiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.group6b.shopiifoodwebsite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopiifoodwebsiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
